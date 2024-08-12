c7dea55a9fce6b6e357a540bca60a4c5492b5ddf
Clare <techclare6@gmail.com>
2024-08-12 01:56:40 +0100

    bonus exercise

--- script.js
+++ script.js
@@ -62,3 +62,66 @@ printGoals(...game.scored)
 
 // check if team1 < team2
 team1 < team2 && console.log("Team 1 is more likely to win")
+
+// coding challenge 2
+// let scoredGoals = game.scored
+// let numGoals = scoredGoals.length - scoredGoals.length
+// console.log(scoredGoals);
+// for(const goals of scoredGoals){
+//   numGoals += 1
+//   console.log(`Goal ${numGoals}: ${goals} `);
+
+// }
+let entries1 = game.scored.entries()
+
+for(let [key, value] of Object.entries(game.scored)){
+  console.log(`Goal ${Number(key) + 1}: ${value} `); 
+  
+}
+
+// calculate the average odd
+let avgOdds = 0;
+let odds = Object.values(game.odds)
+
+for(const odd of odds){
+  avgOdds += odd
+  avgOdds /= odds.length
+  console.log(avgOdds);
+  
+}
+
+// 3
+const threeOdds = Object.entries(game.odds);
+for(const [team, odd] of threeOdds){
+  const teamString = team === "x" ? "draw" : `victory ${game[team]}`
+  // console.log(team);
+  
+  console.log(`Odd of ${teamString}: ${odd}`);
+  
+}
+
+// bonus exercise
+// const scorers ={
+//   Gnarby: 1,
+//   Hummels:1,
+//   Lewandowski:2
+// }
+
+// const scorers = {};
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+//   console.log(scorers[player]);
+  
+// }
+
+//  console.log(scorers);
+
+const  scored = ["Lewandoski", "Gnarby", "Lewandoski", "Hummels"]
+const actualGoals = {}
+
+for(const value of scored){
+  actualGoals[value] ? actualGoals[value]++ : actualGoals[value] = 1
+  // console.log(actualGoals);
+   
+}
+console.log(actualGoals);
